62edde27eed353d617e6b4992a7ffcaef0fa2ac9
Mrugendra <mrugendra.shilvant@foreignadmits.com>
2023-07-31 17:12:11 +0530

    created service

--- src/app/aadhar-verification/aadhar-verification.component.ts
+++ src/app/aadhar-verification/aadhar-verification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-aadhar-verification',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AadharVerificationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
   }
